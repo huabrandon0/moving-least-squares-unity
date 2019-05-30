cffb45e4d08a07392f7761bf982be10bd16e65f6
Brandon Hua <huabrandon0@gmail.com>
2019-05-30 12:17:45 -0700

    Initial commit
    Basic working implementation of moving least squares

---
+++ Assets/Stuffs/MLS.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using Sirenix.OdinInspector;
+
+public class MLS
+{
+    public delegate Matrix MatrixEvaluator(double x);
+    MatrixEvaluator BasisMatrix { get; set; }
+    
+    public delegate double REvaluator(double x);
+    REvaluator WeightFunction { get; set; }
+
+    [ShowInInspector]
+    List<Vector<double>> DataPoints { get; set; }
+    
+    const double SCALING_FACTOR_MIN = 1d + double.Epsilon;
+    const double SCALING_FACTOR_MAX = double.MaxValue;
+
+    [ShowInInspector]
+    double _scalingFactor;
+    public double ScalingFactor
+    {
+        get { return _scalingFactor; }
+        set { _scalingFactor = Mathf.Clamp((float)value, (float)SCALING_FACTOR_MIN, (float)SCALING_FACTOR_MAX); }
+    }
+
+    public MLS(MatrixEvaluator basisMatrix, List<Vector<double>> dataPoints, REvaluator weightFunction, double scalingFactor = SCALING_FACTOR_MIN)
+    {
+        BasisMatrix = basisMatrix;
+        DataPoints = dataPoints.ToList();
+        WeightFunction = weightFunction;
+        ScalingFactor = scalingFactor;
+    }
+
+    public double Evaluate(double x)
+    {
+        List<double> weights = Enumerable.Repeat(0d, DataPoints.Count).ToList();
+
+        int basisLength = BasisMatrix(1).RowCount;
+        Matrix<double> m = Matrix<double>.Build.Dense(basisLength, basisLength);
+        
+        for (int i = 0; i < DataPoints.Count; i++)
+        {
+            double phi = WeightFunction((x - DataPoints[i][0]) / ScalingFactor); // phi(x - x_i / s)
+            weights[i] = phi;
+            
+            Matrix<double> p_xi = BasisMatrix(DataPoints[i][0]);
+            m += phi * p_xi * p_xi.Transpose(); // M += phi(x - x_i / s) * p(x_i) * p(x_i)^t
+        }
+        
+        Matrix<double> mInverse = m.Inverse(); // M^-1
+        Matrix<double> p_x_t = BasisMatrix(x).Transpose(); // p(x)^t
+
+        double p_n_x = 0f; // p_n(x)
+        
+        for (int i = 0; i < DataPoints.Count; i++)
+        {
+            Matrix<double> p_xi = BasisMatrix(DataPoints[i][0]);
+            double m_i = (weights[i] * p_x_t * mInverse * p_xi)[0,0]; // M_i(x) = phi(x - x_i / s) * p(x)^t * M^-1 * p(x_i)
+            p_n_x += DataPoints[i][1] * m_i; // p_n(x) += y_i * M_i(x)
+        }
+        
+        return p_n_x;
+    }
+}
---
+++ Assets/Stuffs/TestBehaviour.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using Sirenix.OdinInspector;
+using System;
+using System.Linq;
+
+public class TestBehaviour : SerializedMonoBehaviour
+{
+    [ShowInInspector]
+    MLS MLS;
+
+    [SerializeField]
+    LineRenderer LineRenderer { get; set; }
+
+    [SerializeField]
+    List<double> EvaluationPoints { get; set; }
+
+    bool Pause { get; set; } = false;
+
+    float TimeOffset { get; set; } = 0f;
+
+    [SerializeField]
+    public double ScalingFactorMin { get; set; } = 1.01f;
+
+    [SerializeField]
+    public double ScalingFactorMax { get; set; } = 50f;
+
+    void Awake()
+    {
+        Matrix LinearBasisMatrix(double x)
+        {
+            return DenseMatrix.OfArray(new double[,] {
+                { 1 },
+                { x }
+            });
+        }
+
+        double WeightFunction(double z)
+        {
+            double phi;
+
+            double zAbs = Math.Abs(z); // Take the absolute value of z
+
+            if (zAbs <= 0.5f)
+                phi = 1f - 6f * Math.Pow(zAbs, 2f) + 6f * Math.Pow(zAbs, 3f);
+            else if (zAbs <= 1f)
+                phi = 2f - 6 * zAbs + 6 * Math.Pow(zAbs, 2f) - 2 * Math.Pow(zAbs, 3f);
+            else
+                phi = 0f;
+
+            return phi;
+        }
+
+        List<Vector<double>> dataPoints = new List<Vector<double>>()
+        {
+            DenseVector.OfArray(new double[] { 1d, 1.4d }),
+            DenseVector.OfArray(new double[] { 2d, 2.3d }),
+            DenseVector.OfArray(new double[] { 3d, 1.7d }),
+            DenseVector.OfArray(new double[] { 4d, 1.9d }),
+            DenseVector.OfArray(new double[] { 5d, 2.7d }),
+            DenseVector.OfArray(new double[] { 6d, 1.6d }),
+            DenseVector.OfArray(new double[] { 7d, 3.3d }),
+            DenseVector.OfArray(new double[] { 8d, 2.2d }),
+            DenseVector.OfArray(new double[] { 9d, 2.5d }),
+            DenseVector.OfArray(new double[] { 10d, 1.9d }),
+        };
+
+        MLS = new MLS(LinearBasisMatrix, dataPoints, WeightFunction);
+
+        EvaluationPoints = Enumerable.Range(0, 180).Select(i => 1 + i * 0.05d).ToList();
+    }
+
+    IEnumerator OscillateScalingFactor()
+    {
+        float startTime = Time.time;
+
+        while (true)
+        {
+            float currentTime = Time.time + TimeOffset;
+
+            float time = currentTime - startTime;
+
+            float interpVal = (float)(0.5d + -Math.Cos(time) * 0.5d);
+
+            SetScalingFactor(Mathf.Lerp((float)ScalingFactorMin, (float)ScalingFactorMax, interpVal));
+
+            yield return null;
+        }
+    }
+
+    [Button]
+    public void StartOscillating()
+    {
+        if (Pause)
+            PauseOscillation();
+
+        StopAllCoroutines();
+        StartCoroutine(OscillateScalingFactor());
+    }
+
+    [Button]
+    public void PauseOscillation()
+    {
+        Pause = !Pause;
+        if (Pause)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    [Button]
+    public void StepBack()
+    {
+        TimeOffset += 0.05f;
+    }
+
+    [Button]
+    public void StepForward()
+    {
+        TimeOffset -= 0.05f;
+    }
+
+    [Button]
+    public void StopOscillating()
+    {
+        StopAllCoroutines();
+    }
+    
+    [Button]
+    public void SetScalingFactor(double s)
+    {
+        MLS.ScalingFactor = s;
+        SetLineRendererPoints();
+    }
+
+    [Button]
+    public void EvaluateMLS(double x)
+    {
+        Debug.Log(MLS.Evaluate(x));
+    }
+
+    public void SetLineRendererPoints()
+    {
+        List<Vector3> positions = new List<Vector3>();
+
+        for (int i = 0; i < EvaluationPoints.Count; i++)
+            positions.Add(new Vector3((float)EvaluationPoints[i], (float)MLS.Evaluate(EvaluationPoints[i]), 0f));
+        
+        LineRenderer.positionCount = positions.Count;
+        LineRenderer.SetPositions(positions.ToArray());
+    }
+}
